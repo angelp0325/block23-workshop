9f3f72f0f90ca7597f44a53b807b7dd76c4fc55f
Angel Polanco <angelepolanco325@gmail.com>
2025-11-11 19:43:09 -0500

    fixed App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { episodeList } from "./data.js";
+import { episodeList } from "./data";
 import "./index.css";
 export default function App() {
   const [episodes] = useState(episodeList);
@@ -7,29 +7,33 @@ export default function App() {
 
   return (
     <div className="app-container">
-      <h1 className="app-title">Dark Echoes</h1>
-
-      <ul className="episode-list">
-        {episodes.map((episode) => (
-          <li
-            key={episode.id}
-            className="episode-item"
-            onClick={() => setSelectedEpisode(episode)}
-          >
-            {episode.title}
-          </li>
-        ))}
-      </ul>
+      <div>
+        <h1 className="app-title">Dark Echoes</h1>
+        <h2 className="subheading">Episodes</h2>
+        <ul className="episode-list">
+          {episodes.map((episode) => (
+            <li
+              key={episode.id}
+              className={`episode-item ${
+                selectedEpisode?.id === episode.id ? "selected" : ""
+              }`}
+              onClick={() => setSelectedEpisode(episode)}
+            >
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <div className="episode-details">
         {!selectedEpisode ? (
           <p className="message">Select an episode to view its details.</p>
         ) : (
           <div>
-            <h2>
-              Episode {selectedEpisode.id}: {selectedEpisode.title}
-            </h2>
+            <h2>Episode {selectedEpisode.id}</h2>
+            <h3>{selectedEpisode.title}</h3>
             <p>{selectedEpisode.description}</p>
+            <button>Watch now</button>
           </div>
         )}
       </div>
